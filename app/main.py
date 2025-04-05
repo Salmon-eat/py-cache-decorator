60e92a7b6186d17797d97c408a3a82d80b75b4e4
Vadym Vizirov <vadimplus57@gmail.com>
2025-04-05 14:32:45 +0300

    result

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+from keyword import kwlist
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+    def wrraper (*args, **kwargs):
+        key = args
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+            return result
+    return wrraper
